34c6a50d266065e0cf38b3a447ced889b806c58a
brunnerth <59923482+brunnerth@users.noreply.github.com>
2020-05-26 14:23:08 +0200

    Update TestTachesPlanifiees.ps1
    Rajout du choix quotitien ou non, rajout du choix de l'heure

--- commandes/TestTachesPlanifiees.ps1
+++ commandes/TestTachesPlanifiees.ps1
@@ -18,18 +18,49 @@
     
   	
 .EXAMPLE
-    
+    .\TestTachesPlanifiees.ps1
+    TaskPath                                       TaskName                          State     
+    --------                                       --------                          -----     
+    \                                              Chrome 637260995116706949         Ready
 #>
 <#
-# Méthode permetant de récupérer les applications au démarrage
+# Méthode permetant de palanifier l'ouverture de chrome sur un site web a une heure précise
 #
 # @param $site Lien vers le site souhaiter
+# @param $heure Heure souhiaiter pour l'ouverture de chrome
+# @param $daily Si la taches s'efefctue chaque jour
 # @retrun -
 #>
-function SchedulTaskChrome($site){
+function SchedulTaskChrome(){
+    param(
+    $site
+    ,$heure
+    ,$daily
+    )
+    $description = "";
     $action= New-ScheduledTaskAction -Execute "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -Argument $site;
-    $trigger= New-ScheduledTaskTrigger -Once -At 14:12;
+    if($daily)
+    {
+        $trigger= New-ScheduledTaskTrigger -Daily -At $heure;
+        $description = "Ouverture de chrome sur un site tous les jours";
+    }
+    else
+    {
+        $trigger= New-ScheduledTaskTrigger -Once -At $heure;
+        $description = "Ouverture de chrome sur un site une seule foi";
+    }
     $taskName = "Chrome " + (get-date).ticks;
-    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description Test;
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description $description;
 }
-SchedulTaskChrome("https://www.adkami.com/");
+$siteChrome = "https://www.adkami.com/";
+try
+{
+    $date = [datetime]::parseexact("00:00", 'HH:mm', $nulls);
+}catch [System.FormatExceptions]
+{
+    $date = [datetime]::parseexact("00:00", 'HH:mm', $nulls);
+}
+if(!($siteChrome -eq $null))
+{
+    SchedulTaskChrome -site $siteChrome -heure $date -daily $true;
+}
